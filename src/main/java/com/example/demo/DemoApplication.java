aac5052054f86d2feb52f2dd806f1b8a88bba745
atommnava <atommunoznava@gmail.com>
2025-03-11 07:58:25 -0600

    Initial commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.awt.*;
-
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
-
         SpringApplication.run(DemoApplication.class, args);
     }
 
