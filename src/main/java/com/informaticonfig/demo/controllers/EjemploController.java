4db05750b7092250ea7d69e2abba35444545cb12
atommnava <atommunoznava@gmail.com>
2026-02-25 11:17:45 -0600

    Cambios

--- src/main/java/com/informaticonfig/demo/controllers/EjemploController.java
+++ src/main/java/com/informaticonfig/demo/controllers/EjemploController.java
@@ -1,19 +1,22 @@
 package com.informaticonfig.demo.controllers;
 
+// Importaciones
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import io.micrometer.observation.annotation.ObservationKeyValue;
 
 @Controller
 public class EjemploController {
     @GetMapping("detalles_info")
-    public String info(Model model)
+    public String info(Map<String, Object> model)
     {
         System.out.println("ENTRO EL CONTROLLER");
-        model.addAttribute("titulo", "Servidor en línea");
-        model.addAttribute("servidor", "Informaticonfig");
-        model.addAttribute("ip", "127.0.0.1");
+        model.put("titulo", "Servidor en línea");
+        model.put("servidor", "Informaticonfig");
+        model.put("ip", "127.0.0.1");
         return "detalles_info";
     }
 
