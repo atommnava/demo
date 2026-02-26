9fe97d4ad1930e1309517055043a320a0c546cd1
atommnava <atommunoznava@gmail.com>
2026-02-25 19:42:07 -0600

    Req Mapping

--- src/main/java/com/informaticonfig/demo/controllers/EjemploRestController.java
+++ src/main/java/com/informaticonfig/demo/controllers/EjemploRestController.java
@@ -3,17 +3,16 @@ package com.informaticonfig.demo.controllers;
 import java.util.HashMap;
 // Importaciones
 import java.util.Map;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 // API Rest
 @RestController
 @RequestMapping("/api")
 public class EjemploRestController {
-    
-    @GetMapping("detalles_info2")
+
+    @RequestMapping(path = "/detalles_info2", method=RequestMethod.GET)
     public Map<String, Object> detalles_info2()
     {
         System.out.println("ENTRO EL CONTROLLER");
