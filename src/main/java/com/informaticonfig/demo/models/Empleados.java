151dfc1b62301d65cf1217b0225a0c087778bafa
atommnava <atommunoznava@gmail.com>
2026-02-26 17:50:32 -0600

    Model

---
+++ src/main/java/com/informaticonfig/demo/models/Empleados.java
@@ -0,0 +1,49 @@
+package com.informaticonfig.demo.models;
+
+public class Empleados {
+    private String nombre, apellido, direccion, puesto;
+    private int edad, telefono, id;
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public String getPuesto() {
+        return puesto;
+    }
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public int getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+}
