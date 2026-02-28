f724058471de20bf0079dd0b07cc2e2da593faed
atommnava <atommunoznava@gmail.com>
2026-02-28 12:53:04 -0600

    DTOs

---
+++ src/main/java/com/informaticonfig/demo/models/dto/ClaseDTO.java
@@ -0,0 +1,22 @@
+package com.informaticonfig.demo.models.dto;
+
+public class ClaseDTO {
+    private String titulo, usuario;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    
+}
