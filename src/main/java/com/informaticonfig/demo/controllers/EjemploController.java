83698923c3b8cb5c99e9ba7a7003ded5e7555f32
atommnava <atommunoznava@gmail.com>
2026-02-28 12:58:28 -0600

    Models

--- src/main/java/com/informaticonfig/demo/controllers/EjemploController.java
+++ src/main/java/com/informaticonfig/demo/controllers/EjemploController.java
@@ -8,19 +8,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.informaticonfig.demo.models.Empleados;
-import com.informaticonfig.demo.models.dto.ClaseDTO;
 
 import io.micrometer.observation.annotation.ObservationKeyValue;
 
 @Controller
 public class EjemploController {
     @GetMapping("detalles_info")
-    public ClaseDTO detalles_info(Model model)
+    public String info(Model model)
     {
-        ClassDTO dto = new ClaseDTO();
-        dto.setTitulo("Administrador");
-        dto.setUsuario("Atom");
-        return dto;
+        System.out.println("ENTRO EL CONTROLLER");
+        Empleados empleado1 = new Empleados("Atom", "Nava", "Apple Street", "Practicante",
+         21, 1111, 1);
+        model.addAttribute("Empleado", empleado1);
+        return "detalles_info";
     }
 
 }
--- src/main/java/com/informaticonfig/demo/controllers/EjemploRestController.java
+++ src/main/java/com/informaticonfig/demo/controllers/EjemploRestController.java
@@ -4,11 +4,13 @@ import java.util.HashMap;
 // Importaciones
 import java.util.Map;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.informaticonfig.demo.models.Empleados;
+import com.informaticonfig.demo.models.dto.ClaseDTO;
 
 // API Rest
 @RestController
@@ -16,14 +18,12 @@ import com.informaticonfig.demo.models.Empleados;
 public class EjemploRestController {
     
     @GetMapping(path = "detalles_info2")
-    public Map<String, Object> detalles_info2()
+    public ClaseDTO detalles_info(Model model)
     {
-        Empleados empleado1 = new Empleados("Atom", "Nava", "Apple Street", "Practicante", 21, 1111, 1);
-        System.out.println("ENTRO EL CONTROLLER");
-        Map<String, Object> respuesta = new HashMap<>();
-        respuesta.put("Empleado", empleado1);
-        
-        return respuesta;
+        ClaseDTO dto = new ClaseDTO();
+        dto.setTitulo("Administrador");
+        dto.setUsuario("Atom");
+        return dto;
     }
 
 }
