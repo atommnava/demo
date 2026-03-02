6284d93d014c5b943a2a235477891828949b9084
atommnava <atommunoznava@gmail.com>
2026-03-02 12:25:54 -0600

    ModelAttribute Anotacion

--- src/main/java/com/informaticonfig/demo/controllers/EjemploController.java
+++ src/main/java/com/informaticonfig/demo/controllers/EjemploController.java
@@ -1,5 +1,7 @@
 package com.informaticonfig.demo.controllers;
 
+import java.util.Arrays;
+import java.util.List;
 // Importaciones
 import java.util.Map;
 import org.springframework.stereotype.Controller;
@@ -17,10 +19,15 @@ public class EjemploController {
     public String info(Model model)
     {
         System.out.println("ENTRO EL CONTROLLER");
-        Empleados empleado1 = new Empleados("Atom", "Nava", null, "Practicante",
+        Empleados empleado1 = new Empleados("Atom", "Nava", "Apple St", "Practicante",
          21, 1111, 1);
         model.addAttribute("Empleado", empleado1);
         return "detalles_info";
     }
-
+    public List<Empleados> listaEmpleados()
+    {
+        return Arrays.asList(
+            new Empleados("Maria", "Waltz", "Main St", "Gerente", 30, 32321, 2)
+        );
+    }
 }
