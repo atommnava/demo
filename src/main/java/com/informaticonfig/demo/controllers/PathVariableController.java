959bfbfd2adfcd3c8c2b3cdf9d0cefb272088582
atommnava <atommunoznava@gmail.com>
2026-03-07 15:38:54 -0600

    Anotación Path variable

--- src/main/java/com/informaticonfig/demo/controllers/PathVariableController.java
+++ src/main/java/com/informaticonfig/demo/controllers/PathVariableController.java
@@ -1,5 +1,15 @@
 package com.informaticonfig.demo.controllers;
 
-public class PathVariableController {
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/variable")
+public class PathVariableController {
+    @GetMapping("/pagina1/{mensaje}")
+    public ParametroDTO pagina1(@PathVariable String mensaje){
+        ParametroDTO parametro1 = new ParametroDTO();
+        return parametro1;
+    }
 }
