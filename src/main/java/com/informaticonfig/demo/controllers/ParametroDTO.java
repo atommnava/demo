54be9aa37894ad30833fb159885f006ef5667195
atommnava <atommunoznava@gmail.com>
2026-03-07 15:28:06 -0600

    REQUEST PARAM fundamentos

---
+++ src/main/java/com/informaticonfig/demo/controllers/ParametroDTO.java
@@ -0,0 +1,14 @@
+package com.informaticonfig.demo.controllers;
+
+public class ParametroDTO {
+    private String informacion;
+
+    public String getInformacion() {
+        return informacion;
+    }
+
+    public void setInformacion(String informacion) {
+        this.informacion = informacion;
+    }
+    
+}
---
+++ src/main/java/com/informaticonfig/demo/controllers/RequestParamControllers.java
@@ -0,0 +1,19 @@
+package com.informaticonfig.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/parametros")
+public class RequestParamControllers {
+    @GetMapping("/detalle")
+    public ParametroDTO detalle(@RequestParam String informacion){
+        ParametroDTO parametro1 = new ParametroDTO();
+        parametro1.setInformacion(informacion);
+        return parametro1;
+    }
+    
+}
